de0da89fe146245c3523b8eac7e35e2d18c84358
David-VDH <64843984+David-VDH@users.noreply.github.com>
2023-02-01 16:29:30 +0200

    fixed movement + new input system
    Fixed the bug and moved to new input system

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,32 +1,51 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     // Opting for diagonal movement, so Vertical for forward, backwards
     // A & D for rotation
 
-    public float speed = 1f;
-    private float rotationSpeed = 0.7f;
-    private float movement;
-    private float rotation;
+    public float movementSpeed = 1f;
+    public float rotationSpeed = 720f;
 
-    private void Update()
+    private Rigidbody2D playerRb;
+
+    private Vector2 movementInput;
+    private Vector2 smoothedMovementInput;
+    private Vector2 movementInputSmoothVelocity;
+
+    private void Awake()
     {
-        rotation = Input.GetAxis("Horizontal");
-        movement = Input.GetAxis("Vertical"); 
+        playerRb = GetComponent<Rigidbody2D>();
     }
 
     private void FixedUpdate()
     {
-        MovePlayer();
+        SetPlayerVelocity();
+        RotateInDirectionOfInput();
     }
 
-    private void MovePlayer()
+    private void SetPlayerVelocity()
     {
-        Vector2 direction = new Vector2(rotation * rotationSpeed, movement);
-        transform.position += (Vector3)direction * speed * Time.deltaTime;
-        transform.up = direction;
+        //Makes it less jerky, starts with itself? takes the input from OnMove(), references its speed, and add time interval if change in seconds.
+        smoothedMovementInput = Vector2.SmoothDamp(smoothedMovementInput, movementInput, ref movementInputSmoothVelocity, 0.1f);
+        playerRb.velocity = smoothedMovementInput * movementSpeed;
+    }
+
+    private void RotateInDirectionOfInput()
+    {
+        if (movementInput != Vector2.zero)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(transform.forward, smoothedMovementInput);
+            Quaternion rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+            playerRb.MoveRotation(rotation); //MoveRotation is new rotation method for rigidbody?
+        }
+    }
+
+    private void OnMove(InputValue inputValue) //Automatic method to collect stuff from new inputsystem, onMove is a Unity EventTrigger
+    {
+        movementInput = inputValue.Get<Vector2>(); //Using the inputsystem namespace, this gets the value of all axis input according to th mapping you chose
     }
 }
