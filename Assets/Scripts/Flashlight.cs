441f374046b91924c31f0140b7dc1a65ed1ba0d6
David-VDH <64843984+David-VDH@users.noreply.github.com>
2023-02-04 12:43:09 +0200

    Succesfully clamped light rotation
    finally

--- Assets/Scripts/Flashlight.cs
+++ Assets/Scripts/Flashlight.cs
@@ -4,61 +4,21 @@ using UnityEngine;
 
 public class Flashlight : MonoBehaviour
 {
+    public Transform playerTransform;
+
     void Update()
-    { 
+    {
         Vector3 mousePosition = Input.mousePosition;
         mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
-        //mousePosition.z = 0;
 
-        Vector3 lightDirection = (mousePosition - transform.position).normalized;
+        //create a direction vector, don't forget to add .normalized to match it to a distance of 1 to prevent diagonal mistakes (Thank you mr pythagoras)
+        Vector3 direction = (mousePosition - playerTransform.position).normalized;
 
-        Vector3 playerDirection = new Vector3(0, 0, transform.parent.rotation.z);
+        // Limits rotation in 40 degrees left and right facing forward
+        float angle = Vector2.SignedAngle(playerTransform.up, direction);
+        angle = Mathf.Clamp(angle, -40, 40);
 
-
-        //witness my failed attempts to make this work
-
-        //Debug.Log(playerDirection);
-        //Debug.Log("Light rotation Z is: " + lightSource.transform.rotation.z);
-        //Debug.Log("Light dir is: " + lightDirection);
-
-        //lightSource.transform.rotation = Quaternion.LookRotation(Vector3.forward, lightDirection);
-
-        /*if (lightSource.transform.eulerAngles.z > -45 && lightSource.transform.eulerAngles.z < 45)
-        {
-            lightSource.transform.rotation = Quaternion.LookRotation(Vector3.forward, lightDirection);
-        }
-
-        //Debug.Log(transform.parent.InverseTransformPoint(Camera.main.ScreenToWorldPoint(Input.mousePosition)).y - transform.parent.localPosition.y);
-        */
-
-        //Debug.Log(transform.eulerAngles.z);
-        //Debug.Log(transform.rotation.z);
-
-       /* if (transform.rotation.z < -0.45f)
-        {
-            transform.eulerAngles = new Vector3(0, 0,-44f);
-        }
-        if (transform.rotation.z > 0.45f)
-        {
-            transform.eulerAngles = new Vector3(0, 0, 44f);
-        }*/
-
-
-
-        //calculate the Y and X position of the mouse relative to the player rotation
-        float mouseY = transform.parent.InverseTransformPoint(Camera.main.ScreenToWorldPoint(Input.mousePosition)).y - transform.parent.localRotation.y;
-        float mouseX = transform.parent.InverseTransformPoint(Camera.main.ScreenToWorldPoint(Input.mousePosition)).x - transform.parent.localRotation.x;
-
-        //checks mouse position relative to player rotation is "above" and "not too far left" and "not too far right"
-        if (mouseY > 0 && mouseX > -0.45 && mouseX < 0.45)
-        {
-            Debug.Log("Up of player!");
-            transform.rotation = Quaternion.LookRotation(Vector3.forward, lightDirection);
-        }
-        //testing to check if mouse position is "below"
-        if (mouseY < 0)
-        {
-            Debug.Log("Down of player!");
-        }
+        //Apply clamped rotation
+        gameObject.transform.localRotation = Quaternion.Euler(0, 0, angle);
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,9 +12,7 @@ public class PlayerController : MonoBehaviour
     public float rotationSpeed = 720f; //2 full circles per 1unit ingame?
 
     private Rigidbody2D playerRb;
-    public Transform parentTransform;
     public GameObject lightSource;
-
 
     private Vector2 movementInput;
     private Vector2 smoothedMovementInput;
@@ -32,7 +30,6 @@ public class PlayerController : MonoBehaviour
     {
         SetPlayerVelocity();
         RotateInDirectionOfInput();
-        //PointFlashLightAtMouse();
     }
 
     private void SetPlayerVelocity()
@@ -48,49 +45,16 @@ public class PlayerController : MonoBehaviour
         {
             targetRotation = Quaternion.LookRotation(transform.forward, smoothedMovementInput);
             lookDirection = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
-
-            playerRb.MoveRotation(lookDirection); //MoveRotation is new rotation method for rigidbody?
+            
+            //MoveRotation is new rotation method for rigidbody?
+            playerRb.MoveRotation(lookDirection);
         }
     }
 
-    /*private void PointFlashLightAtMouse()
+    //Automatic method to collect stuff from new inputsystem, onMove is a Unity EventTrigger
+    private void OnMove(InputValue inputValue)
     {
-
-        Vector3 mousePosition = Input.mousePosition;
-        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
-        Vector3 playerPos = transform.position;
-
-        Vector3 lightDirection = (mousePosition - playerPos).normalized;
-
-        Quaternion fullRotation = Quaternion.LookRotation(Vector3.forward, lightDirection);
-
-        float relativeRotation = parentTransform.rotation.eulerAngles.z - fullRotation.eulerAngles.z;
-        relativeRotation = Mathf.Clamp(relativeRotation, 40, -40);
-        Quaternion clampedRotation = Quaternion.Euler(0, 0, relativeRotation);
-
-        lightSource.transform.rotation = clampedRotation;
-
-        Debug.Log("direction: " + lightDirection);
-        Debug.Log("parentTranfsorm.fwd: " + parentTransform.forward);
-        Debug.Log("fullRotation.eul.z: " + fullRotation.eulerAngles.z);
-        Debug.Log("direction: " + lightDirection);
-        Debug.Log("Clamped.eul.z: " + clampedRotation.eulerAngles.z);
-        Debug.Log("light: " + lightSource.transform.rotation.eulerAngles.z);
-    }*/
-
-    private void OnMove(InputValue inputValue) //Automatic method to collect stuff from new inputsystem, onMove is a Unity EventTrigger
-    {
-        movementInput = inputValue.Get<Vector2>(); //Using the inputsystem namespace, this gets the value of all axis input according to th mapping you chose
+        //Using the inputsystem namespace, this gets the value of all axis input according to th mapping you chose
+        movementInput = inputValue.Get<Vector2>(); 
     }
-
-    //float zAngle = fullRotation.eulerAngles.z;
-    //if (fullRotation.eulerAngles.z > 40 && fullRotation.eulerAngles.z < 180)
-    //{
-    //    zAngle = 40;
-    //}
-    //else if (fullRotation.eulerAngles.z > 180 && fullRotation.eulerAngles.z < 320)
-    //{
-    //    zAngle = 320;
-    //}
-    //Quaternion clampedRotation = Quaternion.Euler(0, 0, zAngle);
 }
