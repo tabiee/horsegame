122ba30467ad17934cc055a4c7389284f61f4f1f
Tabee <ayvanika@gmail.com>
2023-02-04 13:09:12 +0200

    kelpie light avoidance

--- Assets/Scripts/LightCheck.cs
+++ Assets/Scripts/LightCheck.cs
@@ -5,11 +5,26 @@ using Pathfinding;
 
 public class LightCheck : MonoBehaviour
 {
+    [SerializeField] private AIPath aiPath;
+    [SerializeField] private bool inLight;
+    [SerializeField] private float scitterSpeed = 0.5f;
+
+    private void Update()
+    {
+        if (inLight)
+        {
+            aiPath.transform.localPosition += (transform.right * 2f + transform.up) * Time.deltaTime * scitterSpeed;
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.GetComponent<AIPath>() != null && other.name == "evil")
         {
+            aiPath = other.GetComponent<AIPath>();
             Debug.Log("Kelpie is in the cone!");
+            inLight = true;
+
             other.GetComponent<AIPath>().enabled = false;
         }
     }
@@ -19,11 +34,9 @@ public class LightCheck : MonoBehaviour
         if (other.GetComponent<AIPath>() != null && other.name == "evil")
         {
             Debug.Log("Kelpie left the cone!");
+            inLight = false;
+
             other.GetComponent<AIPath>().enabled = true;
         }
-    }
-    void Update()
-    {
-        
     }
 }
