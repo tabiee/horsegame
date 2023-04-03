4bb452258769ea6b92f199058af8952c805e4e95
Tabee <ayvanika@gmail.com>
2023-04-03 16:31:04 +0300

    limsect scatter from corpse animation

--- Assets/Scripts/TriggerAnimation.cs
+++ Assets/Scripts/TriggerAnimation.cs
@@ -5,18 +5,18 @@ using UnityEngine;
 public class TriggerAnimation : MonoBehaviour
 {
     [SerializeField] private Animator animator;
+    [SerializeField] private float lifetime;
     private bool once = false;
-
+    void Start()
+    {
+    }
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player" && once == false)
         {
             animator.SetTrigger("trigger");
             once = true;
+            Destroy(gameObject, lifetime);
         }
-    }
-    private void Update()
-    {
-        //play a sound here
     }
 }
