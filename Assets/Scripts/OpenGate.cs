d6bc1b813d6b9398a15395ffcffd1f6b79109f35
David Vanderheyden <ad0228@student.jamk.fi>
2023-04-16 20:57:30 +0300

    Items work
    interaction works withe the correct items

--- Assets/Scripts/OpenGate.cs
+++ Assets/Scripts/OpenGate.cs
@@ -1,33 +1,43 @@
 using UnityEngine;
 using SFInventory;
 
-public class OpenGate : MonoBehaviour
+public class OpenGate : MonoBehaviour, IInteractable
 {
     public SFInventoryManager sFInventoryManager;
     public GameObject leftGate;
     public GameObject rightGate;
+    private bool gateOpened = false;
 
     public void Interact()
     {
-        bool keyPresent = false;
-        foreach (SFInventoryCell cell in sFInventoryManager.inventoryCells)
+        if (!gateOpened)
         {
-            if (cell.item != null && cell.item.name == "Key" && cell.itemsCount >= 1)
+            bool keyPresent = false;
+            foreach (SFInventoryCell cell in sFInventoryManager.inventoryCells)
             {
-                keyPresent = true;
-                break;
+                if (cell.item != null && cell.item.name == "Key" && cell.itemsCount >= 1)
+                {
+                    keyPresent = true;
+                    break;
+                }
             }
-        }
 
-        if (keyPresent)
-        {
-            leftGate.transform.Rotate(0, -60, 0);
-            rightGate.transform.Rotate(0, 60, 0);
-            Debug.Log("Key item is present in the inventory with at least 1 amount.");
+            if (keyPresent)
+            {
+                leftGate.transform.Rotate(0, -60, 0);
+                rightGate.transform.Rotate(0, 60, 0);
+                gateOpened = true;
+                Debug.Log("Key item is present in the inventory with at least 1 amount. The gate has been opened.");
+            }
+            else
+            {
+                Debug.Log("Key item is not present in the inventory or its amount is less than 1. The gate cannot be opened.");
+            }
         }
         else
         {
-            Debug.Log("Key item is not present in the inventory or its amount is less than 1.");
+            Debug.Log("The gate has already been opened and cannot be opened again.");
         }
     }
 }
+
